6e4bcb937652392f09e65c0c6ae0d0cd0209bd45
Matias Gudin <stkboy@gmail.com>
2016-08-01 17:59:25 -0300

    adds gulp-watch

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const watch = require('gulp-watch');
 
 gulp.task('babel', () => {
     return gulp.src('src/app.js')
@@ -7,4 +8,11 @@ gulp.task('babel', () => {
             presets: ['es2015']
         }))
         .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    return watch('src/*.js', function(event){
+        gulp.start('babel');
+    });
 });
