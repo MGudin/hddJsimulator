aa98e26cd7c969ace172c2b63324a47cf0283173
Ivan Karl <ivan6258@gmail.com>
2016-08-16 17:53:45 -0300

    extracts scheduler to own file

---
+++ src/scheduler.js
@@ -0,0 +1,44 @@
+class Scheduler
+{
+  constructor(
+    method,
+    context
+  )
+  {
+    if (typeof method === 'undefined')
+    {
+      throw new Error('A Method is Required');
+    }
+    if (typeof context === 'undefined')
+    {
+      throw new Error('A Context is Required');
+    }
+
+    this.method = method;
+    this.context = context;
+
+  }
+
+  * steps(context)
+  {
+
+    if (!typeof context === 'undefined')
+    {
+      this.updateContext(context);
+    }
+
+    while (this.hasUnattendedReqs())
+    {
+      next = this.method.next(this.context);
+      this.updateContext(next);
+      yield this.context;
+    }
+  }
+
+  run() {
+    return [...this.steps()];
+  }
+
+}
+
+export default Scheduler;
--- test/test_scheduler.js
+++ test/test_scheduler.js
@@ -1,37 +1,40 @@
 'use strict';
 
 require('babel-polyfill');
-const root_dir = '../'
-const test = require('tape');
-const sim_lib = require(`${root_dir}src/simulation.js`);
-const alg_lib = require(`${root_dir}src/algorithms.js`);
+const root_dir  = '../'
+const test      = require('tape');
+const sim_lib   = require(`${root_dir}src/simulation.js`);
+const alg_lib   = require(`${root_dir}src/algorithms.js`);
+const sched_lib = require(`${root_dir}src/scheduler.js`);
 
 
 test('Scheduler constructor requires method and simulation', assert => {
-  assert.plan(3);
+
   assert.throws(
-    () => { new sim_lib.Scheduler()},
+    () => { new sched_lib.Scheduler() },
     'A Method is required'
   );
 
   assert.throws(
-    () => { new sim_lib.Scheduler('a_method')},
+    () => { new sched_lib.Scheduler('a_method') },
     'A Context is required'
   );
 
   assert.doesNotThrow(
-    () => { new sim_lib.Scheduler('a_method', 'a_context')}
+    () => { new sched_lib.Scheduler('a_method', 'a_context') }
   );
+
+  assert.end();
 });
 
 test('scheduler instance variables are beeing set', assert => {
-  assert.plan(2);
 
   let simulation = new sim_lib.Simulation();
   let method     = new alg_lib.FCFS;
-  let scheduler  = new sim_lib.Scheduler(method, simulation);
+  let scheduler  = new sched_lib.Scheduler(method, simulation);
 
   assert.equals(typeof scheduler.context, 'object');
-  assert.equals(typeof scheduler.method, 'function');
+  assert.equals(typeof scheduler.method, 'object');
 
+  assert.end();
 });
