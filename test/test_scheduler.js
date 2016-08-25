f2f0cfee4af5224c87cab0337ec6e2b14727b96b
Ivan Karl <ivan6258@gmail.com>
2016-08-24 22:38:43 -0300

    improves Scheduler tests

--- test/test_scheduler.js
+++ test/test_scheduler.js
@@ -3,10 +3,14 @@ const root_dir = '../'
 const test     = require('tape');
 require('babel-polyfill');
 
-const alg_lib    = require(`${root_dir}src/algorithms.js`);
+const FCFS       = require(`${root_dir}src/algorithms.js`).FCFS;
 const Simulation = require(`${root_dir}src/simulation.js`).Simulation;
 const Scheduler  = require(`${root_dir}src/scheduler.js`).Scheduler;
 
+
+function simpleScheduler() {
+  return new Scheduler(FCFS, (new Simulation()));
+}
 
 test('Scheduler constructor requires method and simulation', assert => {
 
@@ -29,20 +33,17 @@ test('Scheduler constructor requires method and simulation', assert => {
 
 test('scheduler instance variables are beeing set', assert => {
 
-  let simulation = new Simulation();
-  let method     = new alg_lib.FCFS;
-  let scheduler  = new Scheduler(method, simulation);
+  let scheduler  = simpleScheduler();
 
   assert.equals(typeof scheduler.context, 'object');
-  assert.equals(typeof scheduler.method, 'object');
+  assert.equals(typeof scheduler.method, 'function');
 
   assert.end();
 });
 
 test('Scheduler basic interface', assert => {
-  let simulation = new Simulation();
-  let method     = new alg_lib.FCFS;
-  let scheduler  = new Scheduler(method, simulation);
+
+  let scheduler  = simpleScheduler();
 
   assert.equals(typeof scheduler.steps, 'function');
   assert.equals(typeof scheduler.run, 'function');
@@ -52,9 +53,7 @@ test('Scheduler basic interface', assert => {
 
 test('Scheduler steps returns a generator', assert => {
 
-  let simulation = new Simulation();
-  let method     = new alg_lib.FCFS;
-  let scheduler  = new Scheduler(method, simulation);
+  let scheduler  = simpleScheduler();
   let generator  = scheduler.steps();
 
   assert.equals(typeof generator, 'object');
@@ -65,9 +64,7 @@ test('Scheduler steps returns a generator', assert => {
 
 test('Scheduler `run` returns a results object', assert => {
 
-  let simulation = new Simulation();
-  let method     = new alg_lib.FCFS;
-  let scheduler  = new Scheduler(method, simulation);
+  let scheduler  = simpleScheduler();
   let results    = scheduler.run();
 
   assert.equals(typeof results, 'object');
