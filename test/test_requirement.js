1949de4e8b6897a32cb61c43684090150ec3b8e6
Ivan Karl <ivan6258@gmail.com>
2016-09-13 00:04:33 -0300

    updates requirements tests

--- test/test_requirement.js
+++ test/test_requirement.js
@@ -1,26 +1,44 @@
 'use strict';
 
-const root_dir = '../'
-const test = require('tape');
-const Requirement = require(`${root_dir}src/simulation.js`).Requirement;
-const PageFault = require(`${root_dir}src/simulation.js`).PageFault;
+const root_dir    = '../'
+const test        = require('tape');
+const sim_lib     = require(`${root_dir}src/simulation.js`);
+const Requirement = sim_lib.Requirement;
+const PageFault   = sim_lib.PageFault;
 
 
+test('Requirement equals other Requirement with same value', assert => {
 
-test('test Requirement#equals', assert => {
-  
   let requirement = new Requirement(100);
   let equalRequirement = new Requirement(100);
-  let differentRequirement = new Requirement(200)
+
+  assert.true(requirement.equals(equalRequirement));
+
   let pageFault = new PageFault(100)
   let equalPageFault = new PageFault(100)
-  let differentPageFault = new PageFault(200)
-  
-  assert.equals(requirement.equals(equalRequirement), true)
-  assert.equals(requirement.equals(differentRequirement), false)
 
-  assert.equals(requirement.equals(PageFault), false)
-  assert.equals(pageFault.equals(equalPageFault), true)
-  assert.equals(pageFault.equals(differentPageFault), false)
+  assert.true(pageFault.equals(equalPageFault));
+
+  assert.end();
+});
+
+test('Requirement doesnt equal a PageFault with same value', assert => {
+
+  let pf  = new PageFault(100)
+  let req = new Requirement(100);
+
+  assert.false(pf.equals(req));
+
+  assert.end();
+});
+
+test('Requirement doesnt equal other Requirement with different value', assert => {
+
+  let pf  = new PageFault(100)
+  let req = new Requirement(100);
+
+  assert.false(req.equals(new Requirement(200)));
+  assert.false(pf.equals(new PageFault(200)));
+
   assert.end();
 });
