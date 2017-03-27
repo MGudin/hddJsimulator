fc5e757e4402881faa0d38b4d3cca6c55aa84496
Ivan Karl <ivan6258@gmail.com>
2017-03-27 13:51:27 -0300

    makes parsers tests more succint

--- test/test_parsers.js
+++ test/test_parsers.js
@@ -4,8 +4,8 @@ const root_dir          = '../'
 const test              = require('tape');
 const sim_lib           = require(`${root_dir}src/simulation.js`);
 const Lot               = sim_lib.Lot;
-const Requirement       = sim_lib.Requirement;
-const PageFault         = sim_lib.PageFault;
+const R                 = sim_lib.Requirement;
+const PF                = sim_lib.PageFault;
 const parsers           = require(`${root_dir}src/parsers.js`);
 const LotParser         = parsers.LotParser;
 const RequirementParser = parsers.RequirementParser;
@@ -16,31 +16,26 @@ test('LotParser dismisses trash input', assert => {
 
     let parsed_lot = LotParser(trash_input);
     assert.true(expected.equals(parsed_lot));
+
     assert.end();
 });
 
 test('LotParser dismisses extra whitespaces', assert => {
     let input = '  54 77   *45   ';
-    let expected = new Lot([
-        new Requirement(54),
-        new Requirement(77),
-        new PageFault(45),
-    ]);
+    let expected = new Lot([new R(54), new R(77), new PF(45)]);
 
     let parsed_lot = LotParser(input);
     assert.true(expected.equals(parsed_lot));
+
     assert.end();
 });
 
 test('LotParser dismisses nonsense data inbetween', assert => {
     let input = ' why o why 54 77 some  *45   ';
-    let expected = new Lot([
-        new Requirement(54),
-        new Requirement(77),
-        new PageFault(45),
-    ]);
+    let expected = new Lot([new R(54), new R(77), new PF(45)]);
 
     let parsed_lot = LotParser(input);
     assert.true(expected.equals(parsed_lot));
+
     assert.end();
 });
