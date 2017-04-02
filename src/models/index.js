b490c105500ebafed533c0af83733cca7b6decf3
chudix <stkboy@gmail.com>
2017-04-02 19:29:43 -0300

    adds index.js to models

---
+++ src/models/index.js
@@ -0,0 +1,20 @@
+import algorithm from './algorithm';
+
+import batch from './batch';
+
+import hdd from './hdd';
+
+import lot from './lot';
+
+import scheduler from './scheduler';
+
+import simulation from './simulation';
+
+export {
+  algorithm,
+  batch,
+  hdd,
+  lot,
+  scheduler,
+  simulation,
+}
